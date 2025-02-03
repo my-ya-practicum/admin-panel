11713cc4185248f507f78b0a81125bbaf30c57ee
Орлов Ярослав <y.orlov@fonmix.ru>
2025-02-03 22:20:49 +0300

    fix

--- src/script/sqlite_to_postgres/load_data.py
+++ src/script/sqlite_to_postgres/load_data.py
@@ -4,7 +4,7 @@ withenv ./.env poetry run python3 ./src/script/sqlite_to_postgres/load_data.py
 
 import asyncio
 
-from script.sqlite_to_postgres.etl_movies import Settings
+from script.sqlite_to_postgres.etl_movies.settings import Settings
 from script.sqlite_to_postgres.etl_movies.postgres_saver import PostgresSaver
 from script.sqlite_to_postgres.etl_movies.sqlite_loader import SQLiteLoader
 from script.sqlite_to_postgres.etl_movies.dto.film_work import FilmWorkDTO, GenreDTO, GenreFilmWorkDTO, PersonDTO
